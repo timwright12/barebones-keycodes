27b7bd7583f9e36c1dad25eedc1082d73b6da569
Tim Wright <timwright12@gmail.com>
2022-06-07 13:32:55 -0400

    Updated packages in demo and set lock file

--- demo/src/demo.js
+++ demo/src/demo.js
@@ -1,11 +1,11 @@
 import { ESCAPE } from '../../index.js';
 
 document.addEventListener( 'keyup', ( e ) => {
-	console.log(e);
+	console.log( e );
 	switch ( e.keyCode ) {
-		case ESCAPE:
-			alert( 'ESCAPE key was pressed' );
-			break;
+			case ESCAPE:
+				alert( 'ESCAPE key was pressed' );
+				break;
 	}
 	
 } );
